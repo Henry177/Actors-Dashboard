e71fbd24fa2ede3a3d8cb75d5fb02b42236795a1
Louis <louis.hancke3@gmail.com>
2016-07-13 07:35:44 +0200

    1.0.1

--- ProjectWorkSpace/DashboardUtils/src/bbd/dashboard/DashboardUtils.java
+++ ProjectWorkSpace/DashboardUtils/src/bbd/dashboard/DashboardUtils.java
@@ -1,8 +1,21 @@
 package bbd.dashboard;
 
+import java.net.NetworkInterface;
+import java.net.SocketException;
+
 public class DashboardUtils {
 
 	public static void log(Object logMessage) {
 		System.out.println(logMessage);
+	}
+	
+	public static String getIPandPort() {
+		try {
+			NetworkInterface tmp = NetworkInterface.getNetworkInterfaces().nextElement();
+			return "http://" + tmp.getInetAddresses().nextElement().toString().substring(1) + ":8080/";
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+		return "";
 	}
 }
