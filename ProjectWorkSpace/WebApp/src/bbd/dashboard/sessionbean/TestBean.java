54c2e9b03e9b2c2cb88f6b6c5ae7e4c554367572
WTFSajuuk <louis.hancke3@gmail.com>
2016-07-20 13:55:16 +0200

    1.0.1

--- ProjectWorkSpace/WebApp/src/bbd/dashboard/sessionbean/TestBean.java
+++ ProjectWorkSpace/WebApp/src/bbd/dashboard/sessionbean/TestBean.java
@@ -1,7 +1,6 @@
 package bbd.dashboard.sessionbean;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
@@ -10,8 +9,11 @@ import javax.faces.bean.SessionScoped;
 import bbd.dashboard.Log;
 import bbd.dashboard.Result;
 import bbd.dashboard.dao.DAOType;
-import bbd.dashboard.dao.ProblemTicketDAO;
-import bbd.dashboard.dao.ProblemTicketDAOFactory;
+import bbd.dashboard.dao.environment.EnvironmentDAO;
+import bbd.dashboard.dao.environment.EnvironmentDAOFactory;
+import bbd.dashboard.dao.problemticket.ProblemTicketDAO;
+import bbd.dashboard.dao.problemticket.ProblemTicketDAOFactory;
+import bbd.dashboard.dto.EnvironmentDTO;
 import bbd.dashboard.dto.ProblemTicketDTO;
 
 /**
@@ -35,15 +37,20 @@ public class TestBean implements Serializable {
 		return name;
 	}
     
-    public List<EnvironmentStatus> getEnvironments() {
-    	List<EnvironmentStatus> tmp = new ArrayList<EnvironmentStatus>();
-    	
-    	tmp.add(new EnvironmentStatus("Prod", "6.0.6.41.34", "Delivered"));
-    	tmp.add(new EnvironmentStatus("PreProd++", "6.0.6.42.6", "Delivered"));
-    	tmp.add(new EnvironmentStatus("PreProd", "6.0.7.9", "Started"));
-    	tmp.add(new EnvironmentStatus("Trunck", "1096.0", "Backend Started"));
-    	
-    	return tmp;
+    public List<EnvironmentDTO> getEnvironments() {
+		Log.infoEnabled = true;
+		Log.info("Start");
+		EnvironmentDAO dao = EnvironmentDAOFactory.getInstance().getDAO(daoType);
+		
+		Result<List<EnvironmentDTO>> result = dao.getEnvironments();
+		
+		if(!result.isError()) {
+			List<EnvironmentDTO> environmentList = result.getValue();
+			return environmentList;
+		}
+		Log.info("end");
+		Log.infoEnabled = false;
+		return null;			
     }
     
     public List<ProblemTicketDTO> getProblems() {
@@ -58,7 +65,9 @@ public class TestBean implements Serializable {
     public void addProblem(ProblemTicketDTO dto) {
     	Log.infoEnabled = true;
     	Log.info("Start");
+    	
     	Log.info("code=" + dto.getCode());
+    	//TODO presist problem
     	Log.info("end");
     	Log.infoEnabled = false;
     }
@@ -68,35 +77,4 @@ public class TestBean implements Serializable {
     	return "06 June 09:15 – Quote not returning";
     }
     
-    public class EnvironmentStatus {
-    	
-    	private String name;    	
-		private String version;
-    	private String status;
-   	
-    	public EnvironmentStatus(String name, String version, String status) {
-			setName(name);
-			setVersion(version);
-			setStatus(status);
-		}
-    	
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public String getVersion() {
-			return version;
-		}
-		public void setVersion(String version) {
-			this.version = version;
-		}
-		public String getStatus() {
-			return status;
-		}
-		public void setStatus(String status) {
-			this.status = status;
-		}
-    }
 }
