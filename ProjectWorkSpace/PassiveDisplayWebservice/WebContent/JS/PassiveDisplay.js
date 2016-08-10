1df644379af61623aa1a6f8e527aa9f900d7e935
WTFSajuuk <louis.hancke3@gmail.com>
2016-08-10 09:45:02 +0200

    update dashboard display

--- ProjectWorkSpace/PassiveDisplayWebservice/WebContent/JS/PassiveDisplay.js
+++ ProjectWorkSpace/PassiveDisplayWebservice/WebContent/JS/PassiveDisplay.js
@@ -3,7 +3,6 @@ var myevents = angular.module('environmentsApp', []);
 myevents.controller("EnvironmentsController", function ($scope, $http) {
     $http.get('http://localhost:8080/PassiveDisplay/Dashboard').
     success(function (data) {
-    	alert(data);
         $scope.environments = data;
     });
 });
