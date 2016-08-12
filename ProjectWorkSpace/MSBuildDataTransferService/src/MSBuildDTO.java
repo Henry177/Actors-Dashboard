d134cb102dabb6e176c568ae7c81ed68d790f158
Pieter <pieter.chris.lr@gmail.com>
2016-08-12 15:20:04 +0200

    Added MSBuild

---
+++ ProjectWorkSpace/MSBuildDataTransferService/src/MSBuildDTO.java
@@ -0,0 +1,27 @@
+
+public class MSBuildDTO
+{
+	private String revision;
+	private String status;
+	
+	public String getRevision()
+	{
+		return revision;
+	}
+	
+	public void setRevision(String revision)
+	{
+		this.revision = revision;
+	}
+	
+	public String getStatus() 
+	{
+		return status;
+	}
+	
+	public void setStatus(String status) 
+	{
+		this.status = status;
+	}
+
+}
