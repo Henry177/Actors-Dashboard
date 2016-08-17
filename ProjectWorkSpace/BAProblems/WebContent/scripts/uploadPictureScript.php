c23029559b574c30ca1875561fc233b1d3a95388
WTFSajuuk <louis.hancke3@gmail.com>
2016-08-17 15:21:08 +0200

    Lates

--- ProjectWorkSpace/BAProblems/WebContent/scripts/uploadPictureScript.php
+++ ProjectWorkSpace/BAProblems/WebContent/scripts/uploadPictureScript.php
@@ -2,7 +2,7 @@
 
 //Upload the picture
 $fileDir = "../images/" . $_FILES["fileName"]["name"];
-
+var_dump($fileDir);
 if (move_uploaded_file($_FILES["fileName"]["tmp_name"], $fileDir)) {
     echo "<p>Uploaded successfully!</p>";
 } else {
