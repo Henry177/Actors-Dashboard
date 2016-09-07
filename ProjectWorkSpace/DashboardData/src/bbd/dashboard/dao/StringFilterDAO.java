a9fac57d8a207c6098cdcfc5c12adeec2eca3cf5
WTFSajuuk <louis.hancke3@gmail.com>
2016-09-07 10:15:40 +0200

    AddStringFilter

---
+++ ProjectWorkSpace/DashboardData/src/bbd/dashboard/dao/StringFilterDAO.java
@@ -0,0 +1,16 @@
+package bbd.dashboard.dao;
+
+import java.util.List;
+
+public class StringFilterDAO {
+
+	public StringFilterDAO(String environmentListFileName) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<String> getFilterList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
